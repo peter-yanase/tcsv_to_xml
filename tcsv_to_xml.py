e7d55d212518c00a2bbee4c934f3967225a13c8a
Peter Yanase <54794619+peter-yanase@users.noreply.github.com>
2021-04-21 23:57:45 +0900

    Added normalization function.
    Brushed up the code and added the normalization function. I consider this script complete now.

--- tcsv_to_xml.py
+++ tcsv_to_xml.py
@@ -1,10 +1,13 @@
 # tcsv_to_xml
 # By Peter Yanase
-# Version: 1.0
+# Version: 1.1
 # Built on: Apr 21, 2021
 
 import os
 import csv
+
+def normalize(value):
+    return str.lower(value).replace(' ', '_')
 
 for file_name in os.listdir():
     if file_name.endswith('tsv'):
@@ -13,33 +16,37 @@ for file_name in os.listdir():
         source_data = csv.reader(open(file_name))
     else:
         continue
-    do_or_not = input('Convert ' + file_name + ' to XML? [y/n]\n')
-    if do_or_not == 'y':
-        first_row = True
-        tags = []
-        collection = input('Name your collection. (Use lowercase and underscore only!)\n')
-        entry = input('Name your entries. (Use lowercase and underscore only!)\n')
-        header = '<?xml version="1.0" encoding="UTF-8"?>\n<' + collection + '>\n'
-        footer = '</' + collection + '>'
-        output_file_name = file_name[:-3] + 'xls'
-        output_contents = open(output_file_name, 'w')
-        output_contents.write(header)
-        for row in source_data:
-            if first_row:
-                for tag in row :
-                    tag = str.lower(tag)
-                    tags.append(tag)
-                    for tag_nr in range(len(tags)):
-                        tags[tag_nr] = tags[tag_nr].replace(' ', '_')
-                first_row = False
-            else:
-                output_contents.write('\t<' + entry + '>\n')
-                for column_nr in range(len(tags)):
-                    output_contents.write('\t\t<' + tags[column_nr] + '>' + row[column_nr] + '</' + tags[column_nr] + '>\n')
-                output_contents.write('\t</' + entry + '>\n')
-        output_contents.write(footer)
-        output_contents.close()
-        print('XML file generated:', output_file_name + '\n')
-    elif do_or_not == 'n':
-        continue
+    while True:
+        do_or_not = input('Convert ' + file_name + ' to XML? [y/n]\n').lower()
+        if do_or_not == 'y':
+            first_row = True
+            tags = []
+            collection = input('Name your collection.\n')
+            collection = normalize(collection)
+            entry = input('Name your entries.\n')
+            entry = normalize(entry)
+            header = '<?xml version="1.0" encoding="UTF-8"?>\n<' + collection + '>\n'
+            footer = '</' + collection + '>'
+            output_file_name = file_name[:-3] + 'xls'
+            output_contents = open(output_file_name, 'w')
+            output_contents.write(header)
+            for row in source_data:
+                if first_row:
+                    for tag in row :
+                        tag = normalize(tag)
+                        tags.append(tag)
+                    first_row = False
+                else:
+                    output_contents.write('\t<' + entry + '>\n')
+                    for column_nr in range(len(tags)):
+                        output_contents.write('\t\t<' + tags[column_nr] + '>' + row[column_nr] + '</' + tags[column_nr] + '>\n')
+                    output_contents.write('\t</' + entry + '>\n')
+            output_contents.write(footer)
+            output_contents.close()
+            print('XML file generated:', output_file_name + '\n')
+            break
+        elif do_or_not == 'n':
+            break
+        else:
+            print('Input y or n\n')
 print('No more CSV or TSV files to convert!')
